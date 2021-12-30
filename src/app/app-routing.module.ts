96b6c2ce4f44517d3dd91c04d766e8f7fe567d1c
shanmugapriyabs <shanmugapriyabs06@gmail.com>
2021-12-30 13:03:24 +0530

    70 binaries uploaded

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./component/login/login.component";
+const routes: Routes = [
+  {
+    path: '', component: LoginComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  }
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import {AppRoutingModule} from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-
+import {LoginComponent} from './component/login/login.component';
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule,AppRoutingModule ],
+  declarations: [ AppComponent, LoginComponent,HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
